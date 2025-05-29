ee5e13494031bbe2fc5fa7e1cc04eeeeb3c93229
GuhStvo <gustavo.asilva86@senacsp.edu.br>
2025-05-28 21:59:51 -0300

    API-AUDIOBOOK-PHP

--- services/conexaoDB.php
+++ services/conexaoDB.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
 class conexaoDB {
     private $host       = "localhost";
     private $db_name    = "audiobookapp";
